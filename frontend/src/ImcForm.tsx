8a69455ec462166f9c739db1695d3e3c36d0b7e1
alva <105307315+alva-p@users.noreply.github.com>
2025-09-05 09:46:31 -0300

    fix import

--- frontend/src/ImcForm.tsx
+++ frontend/src/ImcForm.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-const API_URL = import.meta.env.VITE_API_URL;
+import.meta.env.VITE_API_URL;
 
 interface ImcResult {
   imc: number;
