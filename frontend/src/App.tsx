c2c1609473ec7ddb331ddd11cca2097d2b64e055
alva <105307315+alva-p@users.noreply.github.com>
2025-09-12 12:42:54 -0300

    fix frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react';import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ImcForm from './ImcForm';
 import ImcHistorial from './components/ImcHistorial';
--- frontend/src/components/ImcHistorial.tsx
+++ frontend/src/components/ImcHistorial.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import { useState, useMemo } from "react";
 import { ImcRecord } from '../App';
 
 interface ImcHistorialProps {
@@ -7,11 +7,7 @@ interface ImcHistorialProps {
 }
 
 // Iconos SVG
-const FilterIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
-  </svg>
-);
+
 
 const ClearIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
