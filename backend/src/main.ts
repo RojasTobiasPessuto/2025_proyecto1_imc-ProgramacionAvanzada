b662475129758ce7165843649750e9329db3599d
Tobias <tobiasfabian.08@hotmail.com>
2025-09-25 00:33:34 -0300

    Cambio en la main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://2025-proyecto1-imc-programacion-ava-nu.vercel.app', // tu front en Vercel
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe({
